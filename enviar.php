33cd1386251331a0d95e4dfdd9db8839f3913b35
TorresCH <54654596+TorresCH@users.noreply.github.com>
2020-08-08 07:30:47 -0500

    Add files via upload

--- enviar.php
+++ enviar.php
@@ -7,11 +7,12 @@ $empresa = $_POST["empresa"];
 $tele = $_POST["telefono"];
 $ciudad = $_POST["ciudad"];
 $message = $_POST["mensaje"];
+
 $contenido = "Haz ecibido un nuevo mensaje. \n Nombre: $name \n Empleados: $select \n Correo: \n $email \n Empresa: \n $email \n Teléfono: \n $tele \n Ciudad: \n $ciudad \n Mensaje: \n $message";
 
-mail($destinom, "Contacto:" $contenido);
+mail($destinom, "Contacto:", $contenido);
 header("Location:contacto.html");
-//echo "El mensaje se ha enviado correctamente";
-                                ?>
+echo "El mensaje se ha enviado correctamente";
+?>
 
                                    
